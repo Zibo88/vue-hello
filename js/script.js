8ae41ddf5a77eb6b3335eb12f9a66e87edc25620
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-23 13:07:03 +0200

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var app = new Vue (
+    {
+        el:'#root',
+        data:{
+
+            titlePage: 'Foto dallo spazio',
+            photo: 'https://st.depositphotos.com/1000401/2160/i/600/depositphotos_21604911-stock-photo-nebula-gas-cloud-in-deep.jpg'
+        }
+    }
+)
